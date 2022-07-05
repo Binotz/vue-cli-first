dbf1a64157fd9290d348405b4dd433936dfb68a1
Binotz <binotto.riccardo@gmail.com>
2022-07-05 15:48:39 +0200

    Exercise Done

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="title">
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
